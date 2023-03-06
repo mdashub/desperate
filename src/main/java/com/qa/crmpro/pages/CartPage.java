d905ff60cc80ad114c683a3c7073a033cfec1d3a
MangalaDas <loveyouchopra95@gmail.com>
2023-03-06 11:43:39 +0530

    cart page and test added

---
+++ src/main/java/com/qa/crmpro/pages/CartPage.java
@@ -0,0 +1,5 @@
+package com.qa.crmpro.pages;
+
+public class CartPage {
+
+}
---
+++ src/test/java/com/qa/crmpro/tests/CartTest.java
@@ -0,0 +1,11 @@
+package com.qa.crmpro.tests;
+
+import org.testng.annotations.Test;
+
+public class CartTest {
+
+	@Test
+	public void tesCart() {
+		
+	}
+}
